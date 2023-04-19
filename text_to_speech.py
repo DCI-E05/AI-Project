939c1f62d7e0e9f464582bbbfb77a778591c0d46
1bless <dri6lees@gmail.com>
2023-04-19 19:30:40 +0200

    added method

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,4 +1,3 @@
-from aiohttp import BytesIOPayload
 from gtts import gTTS
 from io import BytesIO
 from pydub import AudioSegment
@@ -6,19 +5,21 @@ from pydub.playback import play
 #sudo apt-get install ffmpeg
 #pip install pydub ,pydub playback ,gtts ,io
 
-language = "ru"
-text = "Hello, my name s anton, every morning i usualy get up at seven o'clock and brush my teeth, then i have Breakfast and go to school by bus"
-speech = gTTS(
-    text=text,
-    lang=language, 
-    slow=False
-)
+def text_to_speech(text, language="en", slow=False):
+    speech = gTTS(
+            text=text,
+            lang=language, 
+            slow=slow
+    )
+    audio_bytes = BytesIO()
+    speech.write_to_fp(audio_bytes)
+    audio_bytes.seek(0)
+    audio_segment = AudioSegment.from_file(audio_bytes, format="mp3")
+    play(audio_segment)
 
-audio_bytes = BytesIO()
-speech.write_to_fp(audio_bytes)
-audio_bytes.seek(0)
 
-audio_segment = AudioSegment.from_file(audio_bytes, format="mp3")
+text = "Hello, my name s Anton. Every morning I usually get up at seven o'clock and brush my teeth, then I have breakfast and go to school by bus."
+text_to_speech(text, language="ru")
 
-play(audio_segment)
+
 
