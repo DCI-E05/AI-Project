d0c1dad4c8626008ac06003a077a9e7946752a9c
SeventhFret <maxxik007@gmail.com>
2023-04-19 14:14:19 +0200

    Modified role

--- gpt_requests.py
+++ gpt_requests.py
@@ -4,6 +4,6 @@
 Imagine you are my friend and we have online videocall,
 and you see my camera. We will speak as a normal friends and 
 in the end of each message I'll send you short json code with certain
-objects, that are persist in my video right now, and you will have to describe or
+objects(e.g. {"objects": ["cup", "snapback hat"]}), that are persist in my video right now, and you will have to describe or
 mention it somehow(e.g. cool cup! or take off your phone, while we on call!)
 """
