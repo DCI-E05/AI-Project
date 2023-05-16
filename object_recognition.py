8c8620a6e159722779cdec35ce732dd958a9e961
Marchuk Maxim <maxxik007@gmail.com>
2023-05-16 14:56:00 +0200

    Deleted drawing of rectangles

--- object_recognition.py
+++ object_recognition.py
@@ -6,7 +6,7 @@ import cv2
 
 
 
-def prepare_image(frame, classes, model, draw_results=True):
+def prepare_image(frame, classes, model):
     blob = cv2.dnn.blobFromImage(frame, size=(300, 300), swapRB=True, crop=False)
     model.setInput(blob)
     output = model.forward()
@@ -20,13 +20,4 @@ def prepare_image(frame, classes, model, draw_results=True):
             class_name = classes[class_id]
             results.append(class_name)
 
-            if draw_results:
-                x1 = int(detection[3] * frame.shape[1])
-                y1 = int(detection[4] * frame.shape[0])
-                x2 = int(detection[5] * frame.shape[1])
-                y2 = int(detection[6] * frame.shape[0])
-
-                # Відображення результатів на кадрі
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, class_name, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
-    return frame, results
+    return results
